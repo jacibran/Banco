4344fabbd5d7bfa6e12c56e40c0b45763f42398c
jacibran <jose_antonio_104@hotmail.com>
2019-05-05 20:00:39 +0200

    ultima actualizacion
    Co-Authored-By: null <jacibran@users.noreply.github.com>

--- js/bolsa.js
+++ js/bolsa.js
@@ -24,18 +24,18 @@ function DIAactual(n) {
 
 function mostrarSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("slide");
-  var puntitos = document.getElementsByClassName("puntitos");
-  if (n > slides.length) {inicioSlides = 1}
-  if (n < 1) {inicioSlides = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  var slides = document.getElementsByClassName("slide");//llama al elemento//
+  var puntitos = document.getElementsByClassName("puntitos");//llama al elemento//
+  if (n > slides.length) {inicioSlides = 1}//n es un valor, el cual va adquiriendo valores y si es mayor que la longittud del elementoa avanzara//
+  if (n < 1) {inicioSlides = slides.length}//n es un valor, el cual va adquiriendo valores y si es menor que la longittud del elementoa retrocedera//
+  for (i = 0; i < slides.length; i++) { //con este funcion haremos que incremente el valor de i para asi avanzar o retoceder//
+      slides[i].style.display = "none";////y con esta parque haremos que se oculte las flechas cuando no esten en función//
   }
   for (i = 0; i < puntitos.length; i++) {
-      puntitos[i].className = puntitos[i].className.replace(" active", "");
+      puntitos[i].className = puntitos[i].className.replace(" active", "");//con esta funcion le damos las propiedades a los puntos y poder avanzar y retroceder por la imagenes según el punto que seleccionemos//
   }
-  slides[inicioSlides-1].style.display = "block";
-  puntitos[inicioSlides-1].className += " active";
+  slides[inicioSlides-1].style.display = "block";//esta funcion fija las flechas de navegación sobre las fotos//
+  puntitos[inicioSlides-1].className += " active";//esta funcion indica que punto esta activo//
 }
 
 function invierte()
@@ -43,3 +43,4 @@ function invierte()
 var inver = document.getElementById("myPopup");
 inver.classList.toggle("show");
 }
+//esta función toma el valor del onclick de manera booleana,por tanto si es verdadero mostrará el elemnto y si es falso no lo mostrará//
